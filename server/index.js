37f3e6618e1a3d09096ef78c96f34c0edb61f7f8
Doug Miller <Djmiller1717@gmail.com>
2020-10-23 19:59:06 -0400

    api routes fixed. able to see all routes

--- server/index.js
+++ server/index.js
@@ -11,21 +11,21 @@ app.use(express.json())
 app.use(express.urlencoded( {extended: false} ))
 
 //use express.static() MAKE SURE THE PATH TO YOUR PUBLIC FOLDER IS RIGHT!
-app.use(express.static(path.join(__dirname,'./public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-  })
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// })
 
 //require in your routes and use them on your api path
 app.use('/api', api)
 
 //404 handler
-app.use(function(req, res, next){
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
-})
+// app.use(function(req, res, next){
+//     // const err = new Error('Not found')
+//     // err.status = 404
+//     next(err)
+// })
 
 //500 handler
 app.use(function(err, req, res, next){

362671568562fda20ac0f9b69a10ccb0eee3e6ea
Cynthia E <cynthia8499@hotmail.com>
2020-10-23 12:20:53 -0500

    updated the navBar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -10,44 +10,20 @@ export default class NavBar extends Component {
     return (
       <div >
         <nav class="navbar is-link" role="navigation" aria-label="main navigation">
-
-
           <div id="navbarBasicExample" class="navbar-menu">
             <div class="navbar-start">
-              <a class="navbar-item">
-                Grace Shockers
-      </a>
-
-              <a class="navbar-item">
-                Home
-      </a>
-
+              <a class="navbar-item">Grace Shockers</a>
+              <a class="navbar-item">Home</a>
               <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
-                  Categories
-        </a>
-
+                <a class="navbar-link">Categories</a>
                 <div class="navbar-dropdown">
-                  <a class="navbar-item">
-                    Men
-          </a>
-                  <a class="navbar-item">
-                    Women
-          </a>
-                  <a class="navbar-item">
-                    Kids
-          </a>
-                  <a class="navbar-item">
-                    Scary
-          </a><a class="navbar-item">
-                    Funny
-          </a>
-                  <a class="navbar-item">
-                    Animals
-          </a><a class="navbar-item">
-                    Movies
-          </a>
-
+                  <a class="navbar-item">Men</a>
+                  <a class="navbar-item">Women</a>
+                  <a class="navbar-item">Kids</a>
+                  <a class="navbar-item">Scary</a>
+                  <a class="navbar-item">Funny</a>
+                  <a class="navbar-item">Animals</a>
+                  <a class="navbar-item">Movies</a>
                 </div>
               </div>
             </div>
@@ -56,29 +32,24 @@ export default class NavBar extends Component {
               <div class="navbar-item">
                 <div class="buttons">
                   <a class="button is-black">
-                    <strong>Register</strong>
-                  </a>
-                  <a class="button is-black">
-                    Log in
-          </a>
+                    <strong>Register</strong></a>
+                  <a class="button is-black">Log in</a>
                 </div>
               </div>
             </div>
           </div>
         </nav>
 
-
-
         <div>
           <div class="sidenav">
-            <a href="#AllCategories">All Costumes</a>
-            <a href="#Adult">Men</a>
-            <a href="#Kids">Women</a>
-            <a href="#Movies">Kids</a>
-            <a href="#AllCategories">Scary</a>
-            <a href="#Adult">Funny</a>
-            <a href="#Kids">Animals</a>
-            <a href="#Movies">Movies</a>
+            <a href="#">All Costumes</a>
+            <a href="#">Men</a>
+            <a href="#">Women</a>
+            <a href="#">Kids</a>
+            <a href="#">Scary</a>
+            <a href="#">Funny</a>
+            <a href="#">Animals</a>
+            <a href="#">Movies</a>
           </div>
         </div>
       </div>

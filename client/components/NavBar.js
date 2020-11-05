c2da875b3c9f18ee0ed6b5a4ba34e22c38de39ad
Cynthia E <cynthia8499@hotmail.com>
2020-11-04 18:59:08 -0600

    it works now plus new styling

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -17,7 +17,7 @@ class NavBar extends Component {
     return (
       <div>
         <div className="navContainer">
-          <nav className="navbar is-fixed-top is-link" role="navigation" aria-label="main navigation" id= 'topNav'>
+          <nav className="navbar is-fixed-top is-link" role="navigation" aria-label="main navigation" id='topNav'>
             <div id="navbarBasicExample" className="navbar-menu">
               <div className="navbar-start">
                 <a className="navbar-item">Grace Shockers</a>
@@ -28,8 +28,8 @@ class NavBar extends Component {
 
               <div className="navbar-end">
                 <div className="navbar-item">
-                  <a className="navbar-item">Welcome, { user.id ? user.firstName : 'Guest' }!</a>
-                  { user.id ? (
+                  <a className="navbar-item">Welcome, {user.id ? user.firstName : 'Guest'}!</a>
+                  {user.id ? (
                     <div className="buttons">
                       <Link to="/home" className="button is-black">Account Settings</Link>
                       <Link to="/login" className="button is-black" onClick={logout}>Log out</Link>
@@ -52,7 +52,7 @@ class NavBar extends Component {
           <Link to="/categories/all">All</Link>
           {
             categories.map((category) => (
-              <Link key={category.id} to={`/categories/${category.title}`}>{category.title}</Link>
+              <Link key={category.id} className="side-nav" to={`/categories/${category.title}`}>{category.title}</Link>
             ))
           }
         </div>
--- client/components/costume/AllCostumes.js
+++ client/components/costume/AllCostumes.js
@@ -4,64 +4,64 @@ import { connect } from 'react-redux';
 import { loadCostumesDispatch } from '../../redux/costumes/allCostumes';
 
 class AllCostumes extends Component {
-  componentDidMount () {
+  componentDidMount() {
     this.props.loadCostumesDispatch()
   };
   render() {
-    const { 
-      categories, selectedCategoryName, costumes 
+    const {
+      categories, selectedCategoryName, costumes
     } = this.props
     const selectedCategory = categories.find((category) => category.title === selectedCategoryName)
     return (
       <div className="costumesList">
         <div>
-          <h5>{selectedCategory ? selectedCategoryName : 'All'} Costumes</h5>
-            {selectedCategory ?
+          <h1>{selectedCategory ? selectedCategoryName : 'All'} Costumes</h1>
+          {selectedCategory ?
             costumes.map((costume) => costume.categoryId === selectedCategory.id ? (
               <div key={costume.id} className="costumes">
                 <div>
                   <Link to={`/costumes/${costume.costumeName}/${costume.id}`}>{costume.costumeName}</Link>
                 </div>
-                  <img src={costume.imageUrl} />
+                <img src={costume.imageUrl} />
                 <div>
                   <Link to={`/costumes/${costume.costumeName}/${costume.id}/${costume.categoryId}/admin`}>
-                    Update
+                    Update &nbsp;
                     {costume.costumeName}
                   </Link>
                 </div>
                 <div>${costume.price}</div>
                 <br />
               </div>
-              )
+            )
               : null)
             :
-              costumes.map((costume) => (
-                <div key={costume.id} className="costumes">
-                  <div>
-                    <Link to={`/costumes/${costume.costumeName}/${costume.id}`}>{costume.costumeName}</Link>
-                  </div>
-                  <img src={costume.imageUrl} />
-                  <div>
-                    <Link to={`/costumes/${costume.costumeName}/${costume.id}/${costume.categoryId}/admin`}>
-                      Update
+            costumes.map((costume) => (
+              <div key={costume.id} className="costumes">
+                <div>
+                  <Link to={`/costumes/${costume.costumeName}/${costume.id}`}>{costume.costumeName}</Link>
+                </div>
+                <img src={costume.imageUrl} />
+                <div>
+                  <Link to={`/costumes/${costume.costumeName}/${costume.id}/${costume.categoryId}/admin`}>
+                    Update &nbsp;
                       {costume.costumeName}
-                    </Link>
-                  </div>
-                  <div>{costume.price}</div>
-                  <br />
-                </ div> 
-              ))
-            }
-          </div>
+                  </Link>
+                </div>
+                <div>${costume.price}</div>
+                <br />
+              </ div>
+            ))
+          }
+        </div>
       </div>
     )
   }
 };
 
 const mapStateToProps = (state, ownProps) => ({
-    selectedCategoryName: ownProps.match.params.name,
-    categories: state.categories,
-    costumes: state.costumes,
+  selectedCategoryName: ownProps.match.params.name,
+  categories: state.categories,
+  costumes: state.costumes,
 });
 
 const mapDispatchToProps = (dispatch) => ({
